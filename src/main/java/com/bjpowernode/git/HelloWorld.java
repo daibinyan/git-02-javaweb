6bf645d4bd8bd7cd65655ddec94efc4b6ef60495
daibinyan <daibinyan@163.com>
2021-10-28 02:36:23 +0800

    dbybranch增加内容aaa

--- src/main/java/com/bjpowernode/git/HelloWorld.java
+++ src/main/java/com/bjpowernode/git/HelloWorld.java
@@ -3,5 +3,6 @@ package com.bjpowernode.git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("dbyBranchaaaaa");
     }
 }
